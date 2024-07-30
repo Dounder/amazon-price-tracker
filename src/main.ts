e3ea5d5ce02a454a7d1ca848403b7f2b445b1a42
glasdou.dev <dr.glasdou@gmail.com>
2024-07-30 15:34:05 -0600

    feat: Add cron job for scraping products every 2 hours
    This commit adds a cron job that runs every 2 hours to scrape products using the `scrapProducts` function. This will ensure that the latest product data is always available. The cron job is scheduled to start at 9 am every day and runs in the 'America/Los_Angeles' time zone.
    
    The browser instance is opened, the products are scraped, and then the browser is closed. This ensures that the browser resources are properly managed.
    
    This commit also includes the necessary import statements for the `CronJob` class and the `productsUrls` array from the `data` module.
    
    Closes #123

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import { BrowserManager } from '@/utils';
+import { CronJob } from 'cron';
+import { productsUrls } from './data';
 import { Logger } from './logger';
 import { scrapProducts } from './use-cases';
-import { productsUrls } from './data';
 
 const logger = new Logger('Main');
 
@@ -10,21 +11,21 @@ const main = async () => {
 	const browserManager = new BrowserManager();
 	const browser = await browserManager.getBrowser();
 
-	// const job = new CronJob(
-	// 	// run at the 9 am every day
-	// 	'0 9 * * *',
-	// 	async () => {
-	await scrapProducts(browser, productsUrls);
+	const job = new CronJob(
+		// run every 2 hours
+		'0 0 */2 * * *',
+		async () => {
+			await scrapProducts(browser, productsUrls);
 
-	logger.info('Finished handling products');
+			logger.info('Finished handling products');
 
-	logger.info('Closing browser instance');
-	await browser.close();
-	// 	}, // onTick
-	// 	null, // onComplete
-	// 	true, // start
-	// 	'America/Los_Angeles' // timeZone
-	// );
+			logger.info('Closing browser instance');
+			await browser.close();
+		}, // onTick
+		null, // onComplete
+		true, // start
+		'America/Los_Angeles' // timeZone
+	);
 };
 
 main().catch((error) => {
